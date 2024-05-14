9f785bba3a08c49bb1a5be033276cfea57522105
Calvin Lee <calvinnleeee@gmail.com>
2024-05-14 09:54:54 -0700

    Updated signup submission.
    Added signupError page to display an error if a userID or email already exists in the database. Added error checking to redirect to this page.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
-// const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,35 +21,35 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({extended: false}));
 
 // Env file / Secrets setup
-// const mongodb_host = process.env.MONGODB_HOST;
-// const mongodb_user = process.env.MONGODB_USER;
-// const mongodb_password = process.env.MONGODB_PASSWORD;
-// const mongodb_database = process.env.MONGODB_DATABASE;
-// const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-// const node_session_secret = process.env.NODE_SESSION_SECRET;
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 // MongoDB setup (maybe move out and use an include to reduce clutter?)
-// const MongoClient = require("mongodb").MongoClient;
-// const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
-// var database = new MongoClient(atlasURI);
-// const userCollection = database.db(mongodb_database).collection('users');
-// var mongoStore = MongoStore.create({
-// 	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-// 	crypto: {
-// 		secret: mongodb_session_secret
-// 	}
-// });
+const MongoClient = require("mongodb").MongoClient;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
+var database = new MongoClient(atlasURI);
+const userCollection = database.db(mongodb_database).collection('users');
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+});
 
 // Other required variable setup
 const saltRounds = 12;    // used for bcrypt password hashing
 
 // Cookies for sessions
-// app.use(session({ 
-//   secret: node_session_secret,
-//   store: mongoStore,
-//   saveUninitialized: false, 
-//   resave: true
-// }));
+app.use(session({ 
+  secret: node_session_secret,
+  store: mongoStore,
+  saveUninitialized: false, 
+  resave: true
+}));
 
 
 // ---------------------------------------------------------------------
@@ -64,7 +64,7 @@ app.get('/', (req, res) => {
   Author: Calvin Lee
   Description: Signup validation and creating a new user in the database.
 */
-app.post('signupSubmit', async (req, res) => {
+app.post('/signupSubmit', async (req, res) => {
   // user variables
   let id = req.body.id;
   let name = req.body.name;
@@ -76,22 +76,28 @@ app.post('signupSubmit', async (req, res) => {
   // var pw = req.body.signup-password;
 
   // Joi validation done on browser side, move on to next step
-  // verify that email does not already exist in the database, fail if it does
+  // verify that email and id do not already exist in the database, fail if it does
   const emailExists = (await userCollection.countDocuments({email: email})) > 0 ? true : false;
+  const idExists = (await userCollection.countDocuments({userid: id})) > 0 ? true : false;
   if (emailExists) {
-    res.render("userExists");
+    res.render("signupError", {err: "email"});
+    return;
+  }
+  else if (idExists) {
+    res.render("signupError", {err: "ID"});
     return;
   }
 
   // create a new user in db with the provided name, email, and password (after encrypting it)
   var hashedpw = await bcrypt.hash(pw, saltRounds);
-  await userCollection.insertOne({ username: name, email: email, password: hashedpw });
+  await userCollection.insertOne({ userid: id, username: name, email: email, password: hashedpw });
 
   // create a session for the user and log them in
   req.session.authenticated = true;
   req.session.name = name;
   req.session.cookie.maxAge = 1000 * 60 * 60 * 24;  // 24 hours
-  // res.redirect();
+  // res.redirect("loggedin page here");
+  console.log("Submission successful");
   return;
 });
 
