4a260c51bd619fa0dce6cd34010c1215d619e982
Calvin Lee <calvinnleeee@gmail.com>
2024-05-14 11:17:44 -0700

    Update login functionality.
    Changed login to use ID instead of email. Implemented database connection to retrieve document and test id/password.
    Created loginError page.

--- public/js/landing.js
+++ public/js/landing.js
@@ -20,16 +20,15 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   event.preventDefault();
 
   // get form inputs
-  var userEmail = document.getElementById("login-email").value;
+  var userID = document.getElementById("login-id").value;
   var userPw = document.getElementById("login-pw").value;
 
   // Joi validation
   const schema = Joi.object({
-    email:  Joi.string().email({
-      minDomainSegments: 2, tlds: { allow: ['com', 'org', 'net', 'ca']}}).required(),
-    pw:     Joi.string().max(20).required()
+    ID: Joi.string().min(6).max(20).alphanum().required(),
+    pw: Joi.string().max(20).required()
   });
-  var validationResult = schema.validate({email: userEmail, pw: userPw});
+  var validationResult = schema.validate({ID: userID, pw: userPw});
 
   // if there is an error, display a meaningful message to the user depending on the error
   if (validationResult.error != null) {
@@ -95,6 +94,8 @@ document.querySelectorAll(".clear-error").forEach((btn) => {
   btn.addEventListener("click", (event) => {
     document.getElementById("loginError").textContent = "";
     document.getElementById("loginError").replaceChildren(document.createElement('br'));
+    document.getElementById("signupError").textContent = "";
+    document.getElementById("signupError").replaceChildren(document.createElement('br'));
   });
 });
 
