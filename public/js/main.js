3bad60d068b641193c7353a0ffb7cf17d63252c0
Calvin Lee <calvinnleeee@gmail.com>
2024-05-23 20:55:13 -0700

    Easter egg update.
    Included an upside-down cap to make the easter egg more obvious. Fixed formatting in main.ejs to be consistent with the rest of the code.

--- public/js/main.js
+++ public/js/main.js
@@ -2,60 +2,62 @@
   Author: Anna Dao
   Description: Fetch facts from the JSON file called facts.json
 */
-    //Start the clickCount at 0, when the user clicks
-    let clickCount = 0;
+//Start the clickCount at 0, when the user clicks
+let clickCount = 0;
 
 
-    // Function to fetch facts from a JSON file
-    const fetchFacts = async (url) => {
-        try {
-            const response = await fetch(url);
-            return await response.json();
-        } catch (error) {
-            console.error(`Error fetching facts from ${url}:`, error);
-            return null;
+// Function to fetch facts from a JSON file
+const fetchFacts = async (url) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching facts from ${url}:`, error);
+    return null;
+  }
+};
+
+
+const displayRandomFact = (fact) => {
+  document.getElementById('factText').innerText = fact.fact;
+  document.getElementById('factSource').innerText = fact.source;
+  // document.getElementById('factImage').innerText = fact.image;
+};
+
+
+// Display facts from facts or other facts.
+const handleFactFetching = async () => {
+  try {
+    const factsData = await fetchFacts('../json/facts.json');
+
+    const otherFactsData = await fetchFacts('../json/otherfacts.json');
+
+    if (factsData && otherFactsData) {
+      const facts = factsData.facts;
+      const otherFacts = otherFactsData.weird_climate_facts;
+
+      // Display initial random fact
+      displayRandomFact(facts[Math.floor(Math.random() * facts.length)]);
+      //event listener, when user clicks more facts, will generate a new fact on the page.
+      document.getElementById('moreFacts').addEventListener('click', () => {
+        clickCount++;
+
+        let easterEgg = document.getElementById("easter");
+        if (clickCount < 5) {
+          
+          displayRandomFact(facts[Math.floor(Math.random() * facts.length)]);
+          easterEgg.style.display = "none";
         }
-    };
-
-
-    const displayRandomFact = (fact) => {
-        document.getElementById('factText').innerText = fact.fact;
-        document.getElementById('factSource').innerText = fact.source;
-        // document.getElementById('factImage').innerText = fact.image;
-    };
-
-
-    // Display facts from facts or other facts.
-    const handleFactFetching = async () => {
-        try {
-            const factsData = await fetchFacts('../json/facts.json');
-
-
-            const otherFactsData = await fetchFacts('../json/otherfacts.json');
-
-
-            if (factsData && otherFactsData) {
-                const facts = factsData.facts;
-                const otherFacts = otherFactsData.weird_climate_facts;
-
-
-                // Display initial random fact
-                displayRandomFact(facts[Math.floor(Math.random() * facts.length)]);
-                //event listener, when user clicks more facts, will generate a new fact on the page.
-                document.getElementById('moreFacts').addEventListener('click', () => {
-                    clickCount++;
-                    if (clickCount < 5) {
-                        displayRandomFact(facts[Math.floor(Math.random() * facts.length)]);
-                    } else {
-                        displayRandomFact(otherFacts[Math.floor(Math.random() * otherFacts.length)]);
-                        clickCount = 0;
-                    }
-                });
-            }
-        } catch (error) {
-            console.error('Error handling facts:', error);
+        else {
+          displayRandomFact(otherFacts[Math.floor(Math.random() * otherFacts.length)]);
+          easterEgg.style.display = "inline-block";
+          clickCount = 0;
         }
-    };
+      });
+    }
+  } catch (error) {
+    console.error('Error handling facts:', error);
+  }
+};
 
-
-    handleFactFetching();
+handleFactFetching();
